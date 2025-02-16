d473a3a505b66b3a7d2163380674537ec16cb93f
Anish <anishtanvar@gmail.com>
2025-02-16 14:46:43 +0530

    Update port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Use the authentication routes correctly
 app.use("/api", authRouter);
